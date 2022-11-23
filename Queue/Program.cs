715aedbbcd5a3ec3d17de15205b2e4cbf16a5b18
anton.kharchenko <anonymousStyleKiller@gmail.com>
2022-11-23 16:57:36 +0200

    Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using Queue.Implementations;
+
+var customQueue = new CustomQueue<int>();
+
+customQueue.Enqueue(1);
+customQueue.Enqueue(2);
+customQueue.Enqueue(3);
+customQueue.Enqueue(9);
+customQueue.Enqueue(4);
+
+Console.WriteLine(customQueue.Dequeue(1));
+Console.WriteLine(customQueue.Peek());
