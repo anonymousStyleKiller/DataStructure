0bf6958801ec425098fbc836399708aaab54e6a5
anton.kharchenko <anonymousStyleKiller@gmail.com>
2022-11-30 17:31:43 +0200

    BST

---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Models;
+
+namespace BinarySearchTree;
+
+public class Tree<T> where T : IComparable
+{
+    public Node<T> Root { get; private set; }
+}
---
+++ Shared/Models/Node.cs
@@ -0,0 +1,27 @@
+﻿namespace Shared.Models;
+
+public class Node<T> : IComparable
+    where T: IComparable
+{
+    public T Data { get; private set; }
+    public Node<T> Left { get; private set; }
+    public Node<T> Right { get;private set; }
+
+    public Node(T data)
+    {
+        Data = data;
+    }
+
+    public Node(T data, Node<T> left, Node<T> right)
+    {
+        Data = data;
+        Left = left;
+        Right = right;
+    }
+    
+    public int CompareTo(object? obj)
+    {
+        if (obj is Node<T>) return Data.CompareTo(obj);
+        throw new ArgumentException("Not comparer types");
+    }
+}
